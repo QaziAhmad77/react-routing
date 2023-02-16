97cfa7e11fb1bb3d5625d75dd242b928742b3798
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-17 02:03:20 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,48 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
 const About = () => {
   const navigate = useNavigate();
   const goToContact = () => {
-    navigate("/contact");
+    let x = true;
+    if (x) {
+      navigate("/contact");
+    } else {
+      navigate("/filter");
+    }
   };
   return (
     <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user">User</NavLink>
+          </li>
+          <li>
+            <NavLink to="/filter">Filter</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/qazi">Qazi</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/ahmad">Ahmad</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/khan">Khan</NavLink>
+          </li>
+        </ul>
+      </nav>
       <section>
-        <h1>About</h1>
+        <h1 style={{ fontSize: "50px" }}>About</h1>
         <button
           onClick={() => {
             goToContact();
@@ -23,6 +56,7 @@ const About = () => {
           go back
         </button>
       </section>
+      <h1 style={{ fontSize: "50px" }}>This is About Page</h1>
     </>
   );
 };
