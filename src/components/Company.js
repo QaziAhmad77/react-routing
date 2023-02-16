9f3f86b3b72a1d58a7d28ad5e04e29b1aa216479
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-17 02:03:30 +0500

    Create Company.js

---
+++ src/components/Company.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Company = () => {
+  return (
+    <div>
+      <h1>This is Company Page</h1>
+      <h1>Company Info</h1>
+    </div>
+  );
+};
+
+export default Company;
