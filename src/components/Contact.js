252311e830cb9550c3ec1c27934f3b01dbc93572
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-17 02:03:35 +0500

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,43 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
 const Contact = () => {
-    const navigate = useNavigate();
-    const goToHome=()=>{
-        navigate("/")
-    }
+  const navigate = useNavigate();
+  const goToHome = () => {
+    navigate("/");
+  };
   return (
     <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user">User</NavLink>
+          </li>
+          <li>
+            <NavLink to="/filter">Filter</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/qazi">Qazi</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/ahmad">Ahmad</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/khan">Khan</NavLink>
+          </li>
+        </ul>
+      </nav>
       <section>
-        <h1>Contact</h1>
+        <h1 style={{ fontSize: "50px" }}>Contact</h1>
         <button onClick={goToHome}>go to Home Page</button>
         <button
           onClick={() => {
@@ -18,6 +46,7 @@ const Contact = () => {
           go back
         </button>
       </section>
+      <h1 style={{ fontSize: "50px" }}>This is Contact Page</h1>
     </>
   );
 };
