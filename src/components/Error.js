1ae06221080e9f28f0439e51772b023824398f2d
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-17 19:12:43 +0500

    Update Error.js

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Error = () => {
+  const navigate = useNavigate();
   return (
-    <>Error Page</>
-  )
-}
+    <>
+      <h1 style={{ fontSize: "60px", marginLeft: "auto,", marginRight: "auto" }}>404 Error</h1>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}>
+        go back
+      </button>
+    </>
+  );
+};
 
-export default Error
+export default Error;
