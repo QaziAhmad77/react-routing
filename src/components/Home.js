785d86d60a7a39b59369a06f5372dc5c19db214c
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-17 19:12:57 +0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,45 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Outlet />
+      <a href="/">Logo</a>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user">User</NavLink>
+          </li>
+          <li>
+            <NavLink to="/filter">Filter</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/qazi">Qazi</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/ahmad">Ahmad</NavLink>
+          </li>
+          <li>
+            <NavLink to="/user/khan">Khan</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contactUs">ContactUs</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <h1 style={{ fontSize: "50px" }}>This is Home Page</h1>
     </>
   );
 };
